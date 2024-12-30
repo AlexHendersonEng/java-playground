4591364e18dbbb45b0a6c5c9d5b4c6bae1702507
Alex Henderson <alexhendersnao@gmail.com>
2024-12-30 19:12:19 -0400

    Added project named final

---
+++ final/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Variables declared with the final keyword cannot be changed
+		// after definition. Variables defined with final are often
+		// named with all capitals
+		final double PI = 3.14159;
+		
+		// Print variable to console
+		System.out.println(PI);
+
+	}
+
+}
