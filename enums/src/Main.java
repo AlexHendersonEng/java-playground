3fed0139c1249534c70b4c67beea32b4e86555ac
Alex Henderson <alexhendersnao@gmail.com>
2025-01-19 13:10:43 +0000

    Added enums project

---
+++ enums/src/Main.java
@@ -0,0 +1,13 @@
+/*
+	Enums: Special class that represents a group of constants.
+*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Define enumeration and print value
+		Level level = Level.HIGH;
+		System.out.println(level);
+	}
+
+}
